e5853e07c50c71edc874ba08ab247c764281d9db
Saeed <104280610+derSaeed99@users.noreply.github.com>
2024-09-07 13:23:51 +0200

    portfolio v1 firebase

---
+++ src/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyATp2jpOhFsP5Rw3CT6vX9hsrpB5EleaCQ",
+  authDomain: "myportfolio-95d09.firebaseapp.com",
+  projectId: "myportfolio-95d09",
+  storageBucket: "myportfolio-95d09.appspot.com",
+  messagingSenderId: "26005049441",
+  appId: "1:26005049441:web:4ef6c695009ad18f44da20",
+  measurementId: "G-KXT9T9N8KC"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
